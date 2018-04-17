3d86074bd6fe0be8ae86fe97278e6abb4eacee9c
Chaya Chaipitakporn <titee15017@gmail.com>
2018-04-17 09:25:27 +0700

    minor change

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 app.get('/', function (req, res) {
-  res.send('hello world')
-})
+    console.log('Request Url:' + req.url);
+	
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "test",
+		password: "test",
+		database: "distribution"
+	});
+	
+	con.query('SELECT * FROM branchs',
+		function(err, rows) {
+			if(err) throw err;
+			console.log(rows[0].idbranchs);
+		}
+	);
+	
+});
+
+var port = process.env.PORT || 3000;
+
+
+app.listen(port, () => console.log('listening on port 3000!'));

2b7509f338140808333140c2948a745d3f943eee
Chaya Chaipitakporn <titee15017@gmail.com>
2018-04-20 07:23:34 +0700

    change to fix customer name

--- controllers/productsEachBranchController.js
+++ controllers/productsEachBranchController.js
@@ -2,7 +2,7 @@ module.exports = function(app, con) {
     
     app.get('/branchs/product_management/:idbranch-:branchname-:customername', function (req, res) {
         
-        con.query('SELECT * FROM branchs_products INNER JOIN products ON branchs_products.idproduct = products.id AND idbranch = "' + req.params.idbranch + '" ORDER BY idproduct;',
+        con.query('SELECT * FROM branchs_products INNER JOIN products ON branchs_products.idproduct = products.id AND branchs_products.idbranch = "' + req.params.idbranch + '";',
             function(err, rows) {
                 if(err) throw err;
                 res.render('pages/product_management', {

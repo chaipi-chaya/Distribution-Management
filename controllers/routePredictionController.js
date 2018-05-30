dc17562780fa317a57f74dcfacf6a7fde86cd423
Chaya Chaipitakporn <titee15017@gmail.com>
2018-05-30 21:10:05 +0700

    done prediction, deal with interface

--- controllers/routePredictionController.js
+++ controllers/routePredictionController.js
@@ -7,9 +7,14 @@ module.exports = function(app, con) {
         con.query('SELECT iddelivery_queue AS id, customers.name AS customer_name, branchs.name AS branch_name, branchs.limitation AS limitation, products.name AS product_name, products.weight_per_bottle AS weight_per_bottle, priority, amount_in_bottle, delivery_queues.idcustomer, delivery_queues.idbranch, delivery_queues.idproduct, branchs.location_lat AS location_lat, branchs.location_lon AS location_lon FROM delivery_queues LEFT JOIN customers ON delivery_queues.idcustomer = customers.idcustomer LEFT JOIN branchs ON delivery_queues.idbranch = branchs.id LEFT JOIN products ON delivery_queues.idproduct = products.id ORDER BY iddelivery_queue;',
             function(err, rows) {
                 if(err) throw err;
-                res.render('pages/route_prediction', {
-                    delivery_queues: rows
-                });
+                con.query('SELECT * FROM trucks',
+                    function(err, trucks) {
+                        res.render('pages/route_prediction', {
+                            delivery_queues: rows,
+                            trucks: trucks
+                        });
+                    }
+                );
             }
         );
 
@@ -20,7 +25,9 @@ module.exports = function(app, con) {
         var queues = req.body;
         var ids = queues.id.split(',');
         var priorities = queues.priorities.split(',');
-        var rounds = queues.rounds;
+        var weights = queues.weights.split(',');
+        var idtrucks = queues.idtrucks.split(',');
+        var rounds = (parseInt(idtrucks.length) * 2) + parseInt(queues.extrarounds);
         var data = [];
         var times = [];
         for (let i = 0; i < ids.length; i++) {
@@ -36,11 +43,12 @@ module.exports = function(app, con) {
         
         var results = [];
         var move_list = [];
-        var prediction = await predict(vectors, rounds, ids, times, priorities);
+        var prediction = await predict(vectors, rounds, ids, times, priorities, weights);
         for (let i = 0; i < prediction.length; i++) {
             var time = prediction[i].time;
             var id = prediction[i].id;
             var priority = prediction[i].priority;
+            var weight = prediction[i].weight;
             var centroid = prediction[i].centroid;
             var cluster = prediction[i].cluster;
             var period = "";
@@ -48,9 +56,7 @@ module.exports = function(app, con) {
             var move = {};
             var result = {};
             // have both morn and even
-            console.log(id);
-            console.log(time);
-            if (time.indexOf("0") && time.indexOf("11")) {
+            if (time.indexOf("0") != -1 && time.indexOf("11") != -1) {
                 var morn = 0;
                 var even = 0
                 for (let j = 0; j < time.length; j++) {
@@ -64,19 +70,27 @@ module.exports = function(app, con) {
                         var index = time.indexOf('11');
                         if (priority[index] == 2) {
                             condition[index] = 1;
+                            move.id = id[index];
+                            move.weight = weight[index];
+                            move.type = "moveToNextDay";
+                            move.cluster = cluster[index];
+                            move.priority = 2;
                             // drop move from id
                             id.splice(index, 1);
+                            weight.splice(index, 1);
                             condition.splice(index, 1);
                             cluster.splice(index, 1);
                         } else if (priority[index] == 3) {
                             condition[index] = 2;
                             move.id = id[index];
+                            move.weight = weight[index];
                             move.type = "mornToEven";
                             move.cluster = cluster[index];
                             move.condition = 2;
                             move.priority = 3;
                             // drop move from id
                             id.splice(index, 1);
+                            weight.splice(index, 1);
                             condition.splice(index, 1);
                             cluster.splice(index, 1);
                         } else if (priority[index] == 4) {
@@ -90,19 +104,26 @@ module.exports = function(app, con) {
                         var index = time.indexOf('0');
                         if (priority[index] == 2) {
                             condition[index] = 1;
+                            move.weight = weight[index];
+                            move.type = "moveToNextDay";
+                            move.cluster = cluster[index];
+                            move.priority = 2;
                             // drop move from id
                             id.splice(index, 1);
+                            weight.splice(index, 1);
                             condition.splice(index, 1);
                             cluster.splice(index, 1);
                         } else if (priority[index] == 3) {
                             condition[index] = 2;
                             move.id = id[index];
+                            move.weight = weight[index];
                             move.type = "evenToMorn";
                             move.cluster = cluster[index];
                             move.condition = 2;
                             move.priority = 3;
                             // drop move from id
                             id.splice(index, 1);
+                            weight.splice(index, 1);
                             condition.splice(index, 1);
                             cluster.splice(index, 1);
                         } else if (priority[index] == 4) {
@@ -113,20 +134,24 @@ module.exports = function(app, con) {
                     
                 }
             // have only morn
-            } else if (time.indexOf("0")) {
+            } else if (time.indexOf(0) != -1) {
                 period = "morn";
             // have only even
-            } else if (time.indexOf("11")) {
+            } else if (time.indexOf(11) != -1) {
                 period = "even";
             // have both
-            } else if (time.indexOf("10")) {
+            } else if (time.indexOf(10) != -1) {
                 period = "both";
+            } else {
+                period = "nan";
             }
             // collect result
             result.id = id;
+            result.weight = weight;
             result.condition = condition;
             result.priority = priority;
             result.centroid = centroid;
+            result.cluster = cluster;
             result.period = period;
             results.push(result);
         }
@@ -162,13 +187,189 @@ module.exports = function(app, con) {
                     }
                 }
             }
-            results[j].id.push(move.id);
-            results[j].priority.push(move.priority);
+            results[moveToResultIndex].id.push(move.id);
+            results[moveToResultIndex].priority.push(move.priority);
         }
         
-        console.log(results);
-        console.log(move_list);
-        res.send(results);
+        // assign trucks and move list
+        var fin = [];
+        var move = {};
+        var move_list = [];
+        var truckUsage = {};
+        while (results.length != 0) {
+            var weightRound = 0;
+            var indexRound = 0;
+            var weightTruck = 0;
+            var indexTruck = 0;
+            var sumWeight = [];
+            /// biggest truck for biggest round 
+            //// find biggest round and bigest truck
+            for (let i = 0; i < results.length; i++) {
+                var sum = results[i].weight.reduce((accumulator, currentValue) => accumulator + currentValue);
+                sumWeight.push(sum);
+                if (sum > weightRound) {
+                    weightRound = sum;
+                    indexRound = i;
+                }
+            }
+            var cannotUse = [];
+            var truckUsage = {};
+            for (let i = 0; i < idtrucks.length; i++) {
+                truckUsage[idtrucks[i]] = [];
+            }
+            while (true) {
+                ///// find biggest truck available
+                for (let i = 0; i < idtrucks.length; i++) {
+                    var truck = queues['truck_'+idtrucks[i]];
+                    if (truckUsage[idtrucks[i]].length < 2 && cannotUse.indexOf(i) == -1) {
+                        if (truck > weightTruck) {
+                            weightTruck = truck;
+                            indexTruck = i;
+                        }
+                    }
+                }
+                ///// check if it okey to use
+                if ((results[indexRound].period == 'morn' && truckUsage[idtrucks[indexTruck]].indexOf('morn') == -1) || (results[indexRound].period == 'even' && truckUsage[idtrucks[indexTruck]].indexOf('even') == -1) || (results[indexRound].period == 'both') ) {
+                    break;
+                } else {
+                    //// no room left
+                    cannotUse.push(indexTruck);
+                                
+                }
+            }
+            //// assing biggest to biggest
+            if (weightTruck >= weightRound) {
+                ///// give it a truck
+                results[indexRound].truck = idtrucks[indexTruck];
+                results[indexRound].vacancy = weightTruck - weightRound;
+                ///// save result
+                fin.push(results[indexRound]);
+            } else {
+            //// remove lowest priority
+                while (weightRound > weightTruck) {
+                    var result = results[indexRound];
+                    var priority = result.priority;
+                    while ((priority[priority.length - 1] == 1 || priority[priority.length - 1] == 2) || (weightRound > weightTruck)) {
+                        if (priority.length == 1) {
+                            ///// if last one and weight still larger than truck and priority 
+                            if (weightRound > weightTruck) {
+                                ///// cut that last one split to two
+                                move.id = result.id[0];
+                                move.weight = result.weight[0] - weightTruck;
+                                move.type = result.period;
+                                move.cluster = result.cluster[0];
+                                move.priority = priority[0];
+                                ///// make new move item
+                                ///// change last one
+                                result.weight[0] = weightTruck;
+                                weightRound = weightTruck;
+                            }
+                            move_list.push(move);
+                            move = {};
+                            break;
+                        }
+                        ///// make new move item
+                        move.id = result.id[priority.length - 1];
+                        move.weight = result.weight[priority.length - 1];
+                        move.type = result.period;
+                        move.cluster = result.cluster[priority.length - 1];
+                        move.priority = priority[priority.length - 1];
+                        move_list.push(move);
+                        move = {};
+                        ///// drop lowest priority
+                        weightRound = weightRound - results[indexRound].weight[priority.length - 1];
+                        result.id.splice(priority.length - 1, 1);
+                        result.weight.splice(priority.length - 1, 1);
+                        result.cluster.splice(priority.length - 1, 1);
+                        result.priority.splice(priority.length - 1, 1);
+                        ///// add new move to move list
+                    }
+                    ///// break if it fit
+                    if (weightTruck >= weightRound) {
+                        ///// give it a truck
+                        result.truck = idtrucks[indexTruck];
+                        result.vacancy = weightTruck - weightRound;
+                        ///// save result
+                        fin.push(result);
+                    }
+               }
+            }
+            ///// drop result
+            results.splice(indexRound, 1);
+        }
+        
+        /// deal with move, same as before but add weight
+        var nextDayList = [];
+        for (let i = 0; i < move_list.length; i++) {
+            var move = move_list[i];
+            var cluster = move.cluster;
+            var moveToResultIndex = null;
+            var distance = 9999;
+            var nextDay = move;
+            if (move.type == "even") {
+                for (let j = 0; j < fin.length; j++) {
+                    if ((fin[j].period == "even" || fin[j].period == "both") && fin[j].vacancy > 0) {
+                        var a = cluster[0] - fin[j].centroid[0];
+                        var b = cluster[1] - fin[j].centroid[1];
+                        var c = Math.sqrt( a*a + b*b );
+                        if (c < distance) {
+                            moveToResultIndex = j;
+                            distance = c;
+                        }
+                    }
+                }
+            } else if (move.type == "morn") {
+                for (let j = 0; j < fin.length; j++) {
+                    if ((fin[j].period == "morn" || fin[j].period == "both") && fin[j].vacancy > 0)  {
+                        var a = cluster[0] - fin[j].centroid[0];
+                        var b = cluster[1] - fin[j].centroid[1];
+                        var c = Math.sqrt( a*a + b*b );
+                        if (c < distance) {
+                            moveToResultIndex = j;
+                            distance = c;
+                        }
+                    }
+                }
+            } else if (move.type == "both") {
+                for (let j = 0; j < fin.length; j++) {
+                    if (fin[j].vacancy > 0) {
+                        var a = cluster[0] - fin[j].centroid[0];
+                        var b = cluster[1] - fin[j].centroid[1];
+                        var c = Math.sqrt( a*a + b*b );
+                        if (c < distance) {
+                            moveToResultIndex = j;
+                            distance = c;
+                        }
+                    }
+                }
+            }
+            
+            //// if weight in move is bigger than vacancy
+            if (moveToResultIndex) {
+                if (move.weight > fin[moveToResultIndex].vacancy) {
+                    /// fill all it can
+                    fin[moveToResultIndex].id.push(move.id);
+                    fin[moveToResultIndex].weight.push(fin[moveToResultIndex].vacancy);
+                    fin[moveToResultIndex].cluster.push(move.cluster);
+                    fin[moveToResultIndex].priority.push(move.priority);
+                    fin[moveToResultIndex].vacancy = 0;
+                    /// leftover add to next day
+                    nextDay.weight -= fin[moveToResultIndex].vacancy;
+                    nextDayList.push(nextDay);
+                } else {
+                    ///// add move to fin
+                    fin[moveToResultIndex].id.push(move.id);
+                    fin[moveToResultIndex].weight.push(move.weight);
+                    fin[moveToResultIndex].cluster.push(move.cluster);
+                    fin[moveToResultIndex].priority.push(move.priority);
+                    fin[moveToResultIndex].vacancy -= move.weight;
+                }
+            } else {
+                //// cannot find vacancy, add to nextDay
+                nextDayList.push(nextDay);
+            }
+        }
+        res.send(fin);
         
     });
     
--- models/predict_route.js
+++ models/predict_route.js
@@ -1,22 +1,25 @@
-module.exports = function(data, trucks, ids, times, priorities) {
+module.exports = function(data, rounds, ids, times, priorities, weights) {
     
     return new Promise((resolve, reject) => {
         var kmeans = require('node-kmeans');
-        kmeans.clusterize(data, {k: trucks}, (err,res) => {
+        kmeans.clusterize(data, {k: rounds}, (err,res) => {
             if (err) console.error(err);
             for (var i = 0; i < res.length; i++) {
                 var id = [];
                 var time = [];
                 var priority = [];
+                var weight = [];
                 var index = res[i].clusterInd;
                 for (let j = 0; j < index.length; j++) {
                     id.push(parseInt(ids[index[j]]));
                     time.push(parseInt(times[index[j]]));
                     priority.push(parseInt(priorities[index[j]]));
+                    weight.push(parseInt(weights[index[j]]));
                 }
                 res[i].id = id;
                 res[i].time = time;
                 res[i].priority = priority;
+                res[i].weight = weight;
             }
             
             resolve(res);
